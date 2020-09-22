72ce984e812b9a89fb0b29be07993a7dc57f5c4d
Priya Power <priyajag72@gmail.com>
2020-09-22 11:23:00 -0600

    Update hospital model spec for relationships to surgeries

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,5 +1,7 @@
 class Hospital < ApplicationRecord
   has_many :doctors
-  
+  has_many :surgery_doctors, through: :doctors
+  has_many :surgeries, through: :surgery_doctors
+
   validates_presence_of :name
 end
