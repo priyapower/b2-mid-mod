e708c2f94d56599fb4d6473b7c6b0e67cba78a28
Priya Power <priyajag72@gmail.com>
2020-09-22 08:57:15 -0600

    Add prior seeds to prepare for setting up after a database exists

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,64 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+# FlightPassenger.destroy_all
+# Passenger.destroy_all
+# Flight.destroy_all
+# Airline.destroy_all
+#
+# schmelta = Airline.create!(name:"Schmelta")
+# punited = Airline.create!(name:"Punited")
+#
+# s001 = schmelta.flights.create!(number:001)
+# s002 = schmelta.flights.create!(number:002)
+# s003 = schmelta.flights.create!(number:003)
+#
+# p100 = punited.flights.create!(number:100)
+# p101 = punited.flights.create!(number:101)
+# p102 = punited.flights.create!(number:102)
+#
+# tim = Passenger.create!(name:"Tim", age:30)
+# brian = Passenger.create!(name:"Brian", age:22)
+# mike = Passenger.create!(name:"Mike", age:25)
+# kat = Passenger.create!(name:"Kat", age:35)
+# jeff = Passenger.create!(name:"Jeff", age:43)
+# leta = Passenger.create!(name:"Leta", age:29)
+# lovisa = Passenger.create!(name:"Lovisa", age:35)
+# priya = Passenger.create!(name:"Priya", age:32)
+# arique = Passenger.create!(name:"Arique", age:27)
+#
+# s001.flight_passengers.create!(passenger:tim)
+# s001.flight_passengers.create!(passenger:brian)
+#
+# s002.flight_passengers.create!(passenger:tim)
+# s002.flight_passengers.create!(passenger:kat)
+# s002.flight_passengers.create!(passenger:mike)
+#
+# s003.flight_passengers.create!(passenger:tim)
+# s003.flight_passengers.create!(passenger:brian)
+# s003.flight_passengers.create!(passenger:jeff)
+# s003.flight_passengers.create!(passenger:mike)
+# s003.flight_passengers.create!(passenger:kat)
+#
+# p100.flight_passengers.create!(passenger:jeff)
+# p100.flight_passengers.create!(passenger:leta)
+# p100.flight_passengers.create!(passenger:lovisa)
+#
+# p101.flight_passengers.create!(passenger:lovisa)
+# p101.flight_passengers.create!(passenger:leta)
+#
+# p102.flight_passengers.create!(passenger:priya)
+# p102.flight_passengers.create!(passenger:arique)
