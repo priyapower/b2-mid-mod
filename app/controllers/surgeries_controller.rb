f0c21819b9dbf12f68952f82e62c618d6d4d13e8
Priya Power <priyajag72@gmail.com>
2020-09-22 10:21:07 -0600

    Update surgeries controller show action with instance of surgery and active record call of surgeries on same day

--- app/controllers/surgeries_controller.rb
+++ app/controllers/surgeries_controller.rb
@@ -1,10 +1,11 @@
 class SurgeriesController < ApplicationController
-  
+
   def index
     @surgeries = Surgery.all
   end
 
   def show
-
+    @surgery = Surgery.find(params[:id])
+    @surgeries_by_day = Surgery.where.not(id:@surgery.id).where(day:@surgery.day)
   end
 end
