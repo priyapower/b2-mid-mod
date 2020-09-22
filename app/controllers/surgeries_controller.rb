e96d7119361836a81d5627dca5ed740e01c04ec6
Priya Power <priyajag72@gmail.com>
2020-09-22 11:00:49 -0600

    Add update doctor action and strong params for doctor name

--- app/controllers/surgeries_controller.rb
+++ app/controllers/surgeries_controller.rb
@@ -8,4 +8,17 @@ class SurgeriesController < ApplicationController
     @surgery = Surgery.find(params[:id])
     @surgeries_by_day = Surgery.where.not(id:@surgery.id).where(day:@surgery.day)
   end
+
+  def update_doctor
+    surgery = Surgery.find(params[:id])
+    add_doctor = Doctor.find_by(name:doctor_params[:doctor_name])
+    surgery.surgery_doctors.create(doctor:add_doctor)
+    redirect_to "/surgeries/#{surgery.id}"
+  end
+
+  private
+
+  def doctor_params
+    params.permit(:doctor_name)
+  end
 end
