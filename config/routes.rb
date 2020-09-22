a85076a4f1d738a7e4bfc6d870ff85ba88be45c0
Priya Power <priyajag72@gmail.com>
2020-09-22 11:54:37 -0600

    Add index routes for surgeries nested under hospital

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :surgeries, only: [:index, :show]
   patch "/surgeries/:id", to: "surgeries#update_doctor"
-  resources :hospitals, only: :index
+  resources :hospitals, only: [:index, :show] do
+    get "/surgeries", to: "hospitals/surgeries#surgery_index"
+  end
 end
